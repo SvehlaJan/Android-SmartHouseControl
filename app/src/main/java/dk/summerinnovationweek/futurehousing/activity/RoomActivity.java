621f4c533388f378413254d075ea44ee47e402ab
pszyjaciel <pszyjaciel@interia.pl>
2014-08-12 20:45:53 +0200

    changes in RoomActivity.java and RoomEntity.java

--- app/src/main/java/dk/summerinnovationweek/futurehousing/activity/RoomActivity.java
+++ app/src/main/java/dk/summerinnovationweek/futurehousing/activity/RoomActivity.java
@@ -2,6 +2,7 @@ package dk.summerinnovationweek.futurehousing.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -13,43 +14,77 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import dk.summerinnovationweek.futurehousing.R;
+import dk.summerinnovationweek.futurehousing.entity.RoomEntity;
 
 
-public class RoomActivity extends ActionBarActivity
-{
+public class RoomActivity extends ActionBarActivity {
 
     Button button;
     ImageView image;
 
-	public static Intent newIntent(Context context)
-	{
-		Intent intent = new Intent(context, RoomActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		return intent;
-	}
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, RoomActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return intent;
+    }
 
 
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-		setupActionBar();
-		setContentView(R.layout.activity_room);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setupActionBar();
+        setContentView(R.layout.activity_room);
 
         addListenerOnButton();
+
+
+        RoomEntity re = new RoomEntity(10, "aName", true, 20);
+        String cpuInfo = re.ReadCPUinfo();
+
+
     }
 
 
     public void addListenerOnButton() {
 
-        final FrameLayout bg = (FrameLayout) findViewById(R.id.fragment_room_content);
-        image = (ImageView) findViewById(R.id.ivLightOn);
+        // final LinearLayout bg = (LinearLayout) findViewById(R.id.fragment_room_content);
+        final TextView tvTemp = (TextView) findViewById((R.id.tvTemp));
+        final SeekBar sb = (SeekBar) findViewById(R.id.seekBar);
+        sb.setProgress(0);
 
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int seekValue = sb.getProgress();
+                sb.setBackgroundColor(Color.rgb(seekValue, seekValue, seekValue));
+                sb.invalidate();
+                String val = Integer.toString(seekValue);
+                tvTemp.setText("Temperature: " + val);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser == true) {
+                    ;
+                }
+            }
+        });
+
+        image = (ImageView) findViewById(R.id.ivLightOn);
         button = (ToggleButton) findViewById(R.id.toggleButton);
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -58,7 +93,7 @@ public class RoomActivity extends ActionBarActivity
             public void onClick(View view) {
                 boolean on = ((ToggleButton) view).isChecked();
                 if (on == true) {
-                   image.setImageResource(R.drawable.bulb_on);
+                    image.setImageResource(R.drawable.bulb_on);
                     // bg.setBackgroundColor(0xFFF3F3F3);
                 } else {
                     image.setImageResource(R.drawable.bulb_off);
@@ -68,84 +103,75 @@ public class RoomActivity extends ActionBarActivity
             }
         });
 
-       }
-	
-	@Override
-	public void onStart()
-	{
-		super.onStart();
-	}
-	
-	
-	@Override
-	public void onResume()
-	{
-		super.onResume();
-	}
-	
-	
-	@Override
-	public void onPause()
-	{
-		super.onPause();
-	}
-	
-	
-	@Override
-	public void onStop()
-	{
-		super.onStop();
-	}
-	
-	
-	@Override
-	public void onDestroy()
-	{
-		super.onDestroy();
-	}
-	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// action bar menu
-		MenuInflater menuInflater = getMenuInflater();
-		menuInflater.inflate(R.menu.menu_simple, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-	
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) 
-	{
-		// action bar menu behaviour
-		switch(item.getItemId()) 
-		{
-			case android.R.id.home:
-				// TODO
-				Intent intent = RoomActivity.newIntent(this);
-				startActivity(intent);
-				return true;
-				
-			case R.id.ab_button_refresh:
-				// TODO
-				return true;
-			
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
-	
-	
-	private void setupActionBar()
-	{
-		ActionBar bar = getSupportActionBar();
-		bar.setDisplayUseLogoEnabled(false);
-		bar.setDisplayShowTitleEnabled(true);
-		bar.setDisplayShowHomeEnabled(true);
-		bar.setDisplayHomeAsUpEnabled(false);
-		bar.setHomeButtonEnabled(true);
-		
-		setSupportProgressBarIndeterminateVisibility(false);
-	}
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // action bar menu
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_simple, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // action bar menu behaviour
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // TODO
+                Intent intent = RoomActivity.newIntent(this);
+                startActivity(intent);
+                return true;
+
+            case R.id.ab_button_refresh:
+                // TODO
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void setupActionBar() {
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayUseLogoEnabled(false);
+        bar.setDisplayShowTitleEnabled(true);
+        bar.setDisplayShowHomeEnabled(true);
+        bar.setDisplayHomeAsUpEnabled(false);
+        bar.setHomeButtonEnabled(true);
+
+        setSupportProgressBarIndeterminateVisibility(false);
+    }
 }
--- app/src/main/java/dk/summerinnovationweek/futurehousing/entity/RoomEntity.java
+++ app/src/main/java/dk/summerinnovationweek/futurehousing/entity/RoomEntity.java
@@ -1,95 +1,104 @@
 package dk.summerinnovationweek.futurehousing.entity;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 
-public class RoomEntity implements Serializable
-{
-	private int mId;
-	private String mName;
-	private boolean mMeasuredIsLightOn;
-	private boolean mInputIsLightOn;
-	private int mMeasuredTemperature;
-	private int mInputTemperature;
-	
+public class RoomEntity implements Serializable {
+    private int mId;
+    private String mName;
+    private boolean mMeasuredIsLightOn;
+    private boolean mInputIsLightOn;
+    private int mMeasuredTemperature;
+    private int mInputTemperature;
 
-	public RoomEntity()
-	{
 
-	}
+    public RoomEntity() {
 
-	public RoomEntity(int id, String name, boolean inputIsLightOn, int measuredTemperature)
-	{
-		mId = id;
-		mName = name;
-		mInputIsLightOn = inputIsLightOn;
-		mMeasuredTemperature = measuredTemperature;
-	}
+    }
 
-	public RoomEntity(RoomEntity origin)
-	{
-		mId = origin.mId;
-		mInputIsLightOn = origin.mInputIsLightOn;
-		mMeasuredTemperature = origin.mMeasuredTemperature;
-		mInputTemperature = origin.mInputTemperature;
-	}
+    public RoomEntity(int id, String name, boolean inputIsLightOn, int measuredTemperature) {
+        mId = id;
+        mName = name;
+        mInputIsLightOn = inputIsLightOn;
+        mMeasuredTemperature = measuredTemperature;
+    }
 
-	public int getId()
-	{
-		return mId;
-	}
+    public RoomEntity(RoomEntity origin) {
+        mId = origin.mId;
+        mInputIsLightOn = origin.mInputIsLightOn;
+        mMeasuredTemperature = origin.mMeasuredTemperature;
+        mInputTemperature = origin.mInputTemperature;
+    }
 
-	public void setId(int id)
-	{
-		mId = id;
-	}
+    public int getId() {
+        return mId;
+    }
 
-	public boolean isInputIsLightOn()
-	{
-		return mInputIsLightOn;
-	}
+    public void setId(int id) {
+        mId = id;
+    }
 
-	public void setInputIsLightOn(boolean isLightOn)
-	{
-		mInputIsLightOn = isLightOn;
-	}
+    public boolean isInputIsLightOn() {
+        return mInputIsLightOn;
+    }
 
-	public int getMeasuredTemperature()
-	{
-		return mMeasuredTemperature;
-	}
+    public void setInputIsLightOn(boolean isLightOn) {
+        mInputIsLightOn = isLightOn;
+    }
 
-	public void setMeasuredTemperature(int measuredTemperature)
-	{
-		mMeasuredTemperature = measuredTemperature;
-	}
+    public int getMeasuredTemperature() {
+        return mMeasuredTemperature;
+    }
 
-	public int getInputTemperature()
-	{
-		return mInputTemperature;
-	}
+    public void setMeasuredTemperature(int measuredTemperature) {
+        mMeasuredTemperature = measuredTemperature;
+    }
 
-	public void setInputTemperature(int inputTemperature)
-	{
-		mInputTemperature = inputTemperature;
-	}
+    public int getInputTemperature() {
+        return mInputTemperature;
+    }
 
-	public String getName()
-	{
-		return mName;
-	}
+    public void setInputTemperature(int inputTemperature) {
+        mInputTemperature = inputTemperature;
+    }
 
-	public void setName(String name)
-	{
-		mName = name;
-	}
+    public String getName() {
+        return mName;
+    }
 
-	public boolean isMeasuredIsLightOn()
-	{
-		return mMeasuredIsLightOn;
-	}
+    public void setName(String name) {
+        mName = name;
+    }
 
-	public void setMeasuredIsLightOn(boolean measuredIsLightOn)
-	{
-		mMeasuredIsLightOn = measuredIsLightOn;
-	}
+    public boolean isMeasuredIsLightOn() {
+        return mMeasuredIsLightOn;
+    }
+
+    public void setMeasuredIsLightOn(boolean measuredIsLightOn) {
+        mMeasuredIsLightOn = measuredIsLightOn;
+    }
+
+
+    public String ReadCPUinfo() {
+        ProcessBuilder cmd;
+        String result = "";
+
+        try {
+            String[] args = {"/system/bin/cat", "/proc/cpuinfo"};
+            cmd = new ProcessBuilder(args);
+
+            Process process = cmd.start();
+            InputStream in = process.getInputStream();
+            byte[] re = new byte[1024];
+            while (in.read(re) != -1) {
+                System.out.println(new String(re));
+                result = result + new String(re);
+            }
+            in.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
 }
